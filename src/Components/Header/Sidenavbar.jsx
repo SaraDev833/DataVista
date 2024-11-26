3890b0d9348ce8d4c88994c928ad774ffc5b9186
Sheikh Sara <sksara706@gmail.com>
2024-11-26 18:36:13 +0600

    done

--- src/Components/Header/Sidenavbar.jsx
+++ src/Components/Header/Sidenavbar.jsx
@@ -24,7 +24,7 @@ const Sidenavbar = ({ showSidebar }) => {
   ];
 
   return (
-    <div className={`h-screen w-[256px] shadow-md bg-white flex-col fixed top-0 left-0 z-50 pt-[16px] px-[28px] lg:flex ${showSidebar ? 'block transition-transform duration-300 ease-in-out' : 'hidden'}`}>
+    <div className={` w-[256px] shadow-md bg-white flex-col fixed top-0 left-0 z-50 pt-[16px] px-[28px] lg:flex ${showSidebar ? 'block transition-transform duration-300 ease-in-out' : 'hidden'}`} style={{ height: '-webkit-fill-available' }}>
       <div className={`transition-transform ease-in-out duration-300 ${showSidebar ? 'pt-2 pl-6' : ''}`}>
         <h2>Data<span className='text-purple'>V</span>ista</h2>
       </div>
@@ -34,7 +34,7 @@ const Sidenavbar = ({ showSidebar }) => {
             <NavLink
               to={item.link}
               key={item.name}
-              className={({ isActive }) => 
+              className={({ isActive }) =>
                 `flex gap-2 items-center justify-start py-3 px-4 rounded-md ${isActive ? 'bg-bgColor text-black' : 'text-black'}`}
             >
               <span className='text-2xl font-medium'>{item.icon}</span>
